9092bcfb12ec63a359f0e682d8ef0b507b04fe7b
Jen Stewart <jenstewart2404@gmail.com>
2021-08-25 21:00:55 +0100

    tests and functionality to create artists, updated artist controller and create-database util

--- tests/artist-create.test.js
+++ tests/artist-create.test.js
@@ -1,17 +1,33 @@
 const { expect } = require('chai');
 const request = require('supertest');
+const getDb = require('../src/services/db');
 const app = require('../src/app');
 
 describe('create artist', () => {
+  let db;
+  beforeEach(async () => (db = await getDb()));
+
+  afterEach(async () => {
+    await db.query('DELETE FROM Artist');
+    await db.close();
+  });
+
   describe('/artist', () => {
     describe('POST', () => {
       it('creates a new artist in the database', async () => {
         const res = await request(app).post('/artist').send({
-          name: 'Tame Impala',
-          genre: 'rock',
+          name: 'Periphery',
+          genre: 'djent',
         });
 
         expect(res.status).to.equal(201);
+
+        const [[artistEntries]] = await db.query(
+          `SELECT * FROM Artist WHERE name = 'Periphery'`
+        );
+
+        expect(artistEntries.name).to.equal('Periphery');
+        expect(artistEntries.genre).to.equal('djent');
       });
     });
   });
