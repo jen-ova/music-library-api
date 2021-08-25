52fb6cf909371293ac167c8e7d09f01c3fe65ada
Jen Stewart <jenstewart2404@gmail.com>
2021-08-25 14:18:10 +0100

    added create and drop database files & confirmed the app connects successfully

---
+++ utils/create-database.js
@@ -0,0 +1,56 @@
+// ```js
+// // utils/create-database.js
+
+const mysql = require('mysql2/promise');
+
+// require path to handle file paths
+const path = require('path');
+
+// extract any command line arguments from argv
+const args = process.argv.slice(2)[0];
+
+// use args to determine if .env or .env.test should be loaded
+const envFile = args === 'test' ? '../.env.test' : '../.env';
+
+// load environment variables from env files
+require('dotenv').config({
+  path: path.join(__dirname, envFile),
+});
+
+// destructure environment variables from process.env
+const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env;
+
+// This asyncronous function will run before app
+const setUpDatabase = async () => {
+  try {
+    // connect to the database
+    const db = await mysql.createConnection({
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      port: DB_PORT,
+    });
+    // create the database if it doesn't already exist
+    await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
+    db.close();
+  } catch (err) {
+    // if something goes wrong, console.log the error and the current environment variables
+    console.log(
+      `Your environment variables might be wrong. Please double check .env file`
+    );
+    console.log('Environment Variables are:', {
+      DB_PASSWORD,
+      DB_NAME,
+      DB_USER,
+      DB_HOST,
+      DB_PORT,
+    });
+    console.log(err);
+  }
+};
+
+// run the async function
+setUpDatabase();
+
+// Comments have been added to help you understand what the code is doing. You can remove them from your version if you prefer.
+// We will add `db.query()` to this file as we add tables to our app. To avoid this turning into callback hell, we have used `async/await` syntax.
---
+++ utils/drop-database.js
@@ -0,0 +1,26 @@
+// ```js
+// // utils/create-database.js
+
+const mysql = require('mysql2');
+
+// require path to handle file paths
+const path = require('path');
+
+// load environment variables from env files
+require('dotenv').config({
+  path: path.join(__dirname, envFile),
+});
+
+// destructure environment variables from process.env
+const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env;
+
+// connect to the database
+const connection = mysql.createConnection({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+});
+
+// teardown the database
+connection.query(`DROP DATABASE ${DB_NAME}`, () => connection.end());
